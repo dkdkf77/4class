19218e16d8b31842977498ffb0720609fe42395a
dam-lee <studymida88@gmail.com>
2021-09-13 23:24:40 +0900

    기본구조업로드

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
+from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request, url_for
 
 app = Flask(__name__)
 
-from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.Yanudu
 
+
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
-    #add commit push
+# add commit push
+# 켜기 터미널
+# set FLASK_APP=app.py
+# set FLASK_ENV=development
+# flask run
+# 끄기 터미널에서 ctrl c
