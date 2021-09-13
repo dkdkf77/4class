a1f66506c7f2abd75451ef3d72ec7148726b2092
dam-lee <studymida88@gmail.com>
2021-09-13 17:13:24 +0900

    gg

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from pymongo import MongoClient
-import requests
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for
+
 app = Flask(__name__)
 
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.Yanudu
 
-# HTML을 주는 부분
 @app.route('/')
-def home():
+def main():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+    #add commit push
