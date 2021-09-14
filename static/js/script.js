192709d853a629dd59b2f77a224dfb6ef3fc6904
dam-lee <studymida88@gmail.com>
2021-09-14 14:21:18 +0900

    로그인 회원가입 html css 작업완료

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,13 @@
-console.log("script");
+const loginBtn = document.querySelector(".login-btn");
+const signupBtn = document.querySelector(".signup-btn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", (e) => {
+    console.log("login btn click");
+  });
+}
+if (signupBtn) {
+  signupBtn.addEventListener("click", (e) => {
+    console.log("signup btn click");
+  });
+}
