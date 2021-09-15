0b6e36a753a7bbee0afcb574b64e4384837b1cf8
dam-lee <studymida88@gmail.com>
2021-09-15 09:29:54 +0900

    signup

--- static/js/signup.js
+++ static/js/signup.js
@@ -85,7 +85,7 @@ function checkText(value) {
 // 아이디 중복 확인 버튼 클릭했는지 확인하는 변수
 let checkId = false;
 // 회원가입 버튼 클릭 했을 때
-signupBtn.addEventListener("click", (e) => {
+signupBtn.addEventListener("click", () => {
   if (signupId.value === "") checkText("signupId");
   if (signupId.value !== "") checkText("signupId-hidden");
   if (signupPw.value === "") checkText("signupPw");
@@ -98,9 +98,25 @@ signupBtn.addEventListener("click", (e) => {
   if (signupTeam.value === "") checkText("signupTeam");
   if (signupTeam.value !== "") checkText("signupTeam-hidden");
   if (isNaN(signupTeam.value)) checkText("signupTeam-number");
-  if (signupId.value !== "" && !checkId) {
+  if (!checkId) {
     checkText("signupId-reg-Btn");
     return null;
+  } else {
+    fetch("/signup/save", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userid_give: signupId.value,
+        password_give: signupPw.value,
+        username_give: signupName.value,
+        userteam_give: signupTeam.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => console.log("Success:", JSON.stringify(response)))
+      .catch((error) => console.error("Error:", error));
   }
 });
 
@@ -117,11 +133,28 @@ idCheckBtn.addEventListener("click", (e) => {
     checkText("signupId-reg-check");
     checkId = true;
   }
-  // checkText("signupId-reg-check");
-  // checkText("signupId-haveId");
+  fetch("/signup/check_dup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userid_give: signupId.value }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.exists) {
+        console.log("중복된 아이디입니다.");
+        checkText("signupId-haveId");
+      } else {
+        console.log("중복되지 않은 아이디입니다.");
+        checkText("signupId-reg-check");
+      }
+    });
 });
 
+// id 값 정규표현식
 function is_id(value) {
+  // 영어로 시작하고 영어+숫자 포함한 6자리 이상 20자리 미만
   const regExp = /^[a-z]+[a-z0-9]{5,19}$/g;
   return regExp.test(value);
 }
