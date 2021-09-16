abb8b75c8520daf0acbd2514228bd6da7971bf30
dam-lee <studymida88@gmail.com>
2021-09-17 07:20:42 +0900

    기능구현 완성

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+const logoutBtn = document.querySelector(".logout-btn");
+logoutBtn.addEventListener("click", () => {
+  document.cookie = `port-token = expires=Thu, 01 Jan 1999 00:00:10 GMT;`;
+  window.location.href = "/";
+});
