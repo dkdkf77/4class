709ced4a2a4d75e55909c48d408efa505f71f3fa
dam-lee <studymida88@gmail.com>
2021-09-18 02:03:24 +0900

    팀 입장시 번호 맞아야 함 안내 문구 추가

--- static/js/roomlist.js
+++ static/js/roomlist.js
@@ -3,7 +3,6 @@ window.addEventListener("load", function () {
 });
 
 function loadList() {
-  console.log("?");
   fetch("/roomlist/number", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
@@ -12,11 +11,12 @@ function loadList() {
     .then((response) => {
       let team = response.team;
       let name = response.name;
+      let id = response.id;
       let roomsArray = response.listArray;
       const roomlistWrap = document.querySelector(".roomlist-wrap");
       roomsArray.map((rooms) => {
         roomlistWrap.innerHTML += `
-                <div class="roomlist ${rooms}"  onclick="userRoomCheck(event,${team},'${name}')">
+                <div class="roomlist ${rooms}"  onclick="userRoomCheck(event,${team},'${name}','${id}')">
                   <p class="room-team-number">${rooms}팀</p>
                 </div>
             `;
@@ -28,13 +28,16 @@ function loadList() {
     });
 }
 
-function userRoomCheck(e, team, name) {
+function userRoomCheck(e, team, name, id) {
   const userRoomNumber = parseInt(team);
   const userName = name;
   let clickRoom = e.currentTarget.className;
   clickRoom = parseInt(clickRoom.split(" ").reverse()[0]);
-
-  if (clickRoom !== userRoomNumber) {
+  // console.log(name);
+  // if (id === "admin1") {
+  //   window.location.href = "/room";
+  // }
+  if (clickRoom !== userRoomNumber && id !== "admin1") {
     alert(
       `${userName} 회원님의 팀 번호는 ${userRoomNumber}팀 입니다. 다른 팀 방에는 입장할 수 없습니다`
     );
